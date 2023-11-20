1c52115ba5cc819c59ec8bfabe411a587b367ced
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-11-21 00:02:40 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/:id",
-        destination: "/:id", // Matched parameters can be used in the destination
+        destination: "/", // Matched parameters can be used in the destination
         permanent: true,
       },
     ];
