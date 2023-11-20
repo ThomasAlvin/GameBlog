11eaad6a95759daa16931f7f7099c6fe5aa7d7d8
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-11-21 00:01:02 +0700

    a

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:slug",
+        destination: "/news/:slug", // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
