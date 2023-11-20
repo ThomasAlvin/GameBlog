13ea7f4c421077acabc5ed12c96d1603001d411b
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-11-21 00:19:59 +0700

    a

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:id",
-        destination: "/", // Matched parameters can be used in the destination
-        permanent: false,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
