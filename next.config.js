378e55c65253fbfb5974d254eed52702552c2ec0
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-11-21 00:01:29 +0700

    a

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/old-blog/:slug",
-        destination: "/news/:slug", // Matched parameters can be used in the destination
+        source: "/:id",
+        destination: "/:id", // Matched parameters can be used in the destination
         permanent: true,
       },
     ];
